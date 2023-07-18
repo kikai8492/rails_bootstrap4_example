315fc560f04233ba1ea0e0422fa2f3dc5207d3d6
shuto kikai <yysskikai04@icloud.com>
2023-07-18 15:06:40 +0900

    commit

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -3,7 +3,7 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('turbolinks:load', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
